394f506902ba6c16b8bb56498b2aeb49be132fc5
Shirisha Palakurla <palakurlashirishagoud@gmail.com>
2026-07-08 23:26:56 +0530

    Update i2c.h

--- Header files/i2c.h
+++ Header files/i2c.h
@@ -1,21 +1,34 @@
-/**
- * **************************************************************************
- * @file    i2c.h
- * @brief   Abstract prototypes for the LPC21xx I2C engine driver layer.
- * **************************************************************************
- */
+// Start header guard
+#ifndef _I2C_H_
 
-#ifndef _I2C_H_                      
-#define _I2C_H_                      
+// Define header guard
+#define _I2C_H_
 
-#include "types.h"                  
+// Include user defined data types
+#include "types.h"
 
-void init_i2c(void);                
-void i2c_start(void);               
-void i2c_stop(void);                
-void i2c_restart(void);             
-void i2c_write(u8 sdat);            
-u8 i2c_mack(void);                  
-u8 i2c_nack(void);                  
 
+// Initialize I2C
+void init_i2c(void);
+
+// Start I2C
+void i2c_start(void);
+
+// Stop I2C
+void i2c_stop(void);
+
+// Restart I2C
+void i2c_restart(void);
+
+// Write one byte
+void i2c_write(u8 sdat);
+
+// Read one byte with ACK
+u8 i2c_mack(void);
+
+// Read one byte without ACK
+u8 i2c_nack(void);
+
+
+// End header guard
 #endif
