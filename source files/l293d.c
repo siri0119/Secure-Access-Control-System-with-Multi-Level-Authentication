7f8170dbdb84e4dc62f7f5766fd2e8c5903279ad
Shirisha Palakurla <palakurlashirishagoud@gmail.com>
2026-07-08 22:53:40 +0530

    Update l293d.c

--- source files/l293d.c
+++ source files/l293d.c
@@ -1,45 +1,44 @@
-/**
- * **************************************************************************
- * @file    l293d.c
- * @brief   H-Bridge Motor Driver Control (Door Actuator Open/Close/Stop Actions).
- * **************************************************************************
- */
-
 #include <lpc21xx.h>
 #include "macros.h"
 #include "l293d.h"
 
-/**
- * @brief  Configures designated GPIO control channels as outputs.
- */
+
+// Initialize motor pins
 void init_l293d(void)
 {
-        IODIR0 |= ((1<<M1) | (1<<M2)); /* Assign H-Bridge control lines to Output mode */
+        // Set M1 and M2 pins as output
+        IODIR0 |= ((1<<M1) | (1<<M2));
 }
 
-/**
- * @brief  Drives the H-Bridge lines to actuate forward rotational movement (Open Door).
- */
+
+// Open the door
 void door_open(void)
 {
-         G_SETBIT(IOSET0,M1);  /* Drive line M1 High */
-         G_SETBIT(IOCLR0,M2);  /* Pull line M2 Low */
+        // Set M1 pin high
+        G_SETBIT(IOSET0,M1);
+
+        // Set M2 pin low
+        G_SETBIT(IOCLR0,M2);
 }
 
-/**
- * @brief  Drives the H-Bridge lines to actuate reverse rotational movement (Close Door).
- */
+
+// Close the door
 void door_close(void)
 {
-         G_SETBIT(IOCLR0,M1);  /* Pull line M1 Low */
-         G_SETBIT(IOSET0,M2);  /* Drive line M2 High */
+        // Set M1 pin low
+        G_SETBIT(IOCLR0,M1);
+
+        // Set M2 pin high
+        G_SETBIT(IOSET0,M2);
 }
 
-/**
- * @brief  Brakes the H-Bridge to stall rotational shaft movement (Stop Door).
- */
+
+// Stop the motor
 void motor_stop(void)
 {
-         G_SETBIT(IOCLR0,M1);  /* Force line M1 Low */
-         G_SETBIT(IOCLR0,M2);  /* Force line M2 Low */
+        // Set M1 pin low
+        G_SETBIT(IOCLR0,M1);
+
+        // Set M2 pin low
+        G_SETBIT(IOCLR0,M2);
 }
