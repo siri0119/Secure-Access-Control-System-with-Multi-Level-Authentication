e99b4b5f57d3b93ef2722689a00f39dd5a03c71f
Shirisha Palakurla <palakurlashirishagoud@gmail.com>
2026-07-08 23:32:53 +0530

    Update i2c_eeprom.h

--- Header files/i2c_eeprom.h
+++ Header files/i2c_eeprom.h
@@ -1,18 +1,49 @@
-/**
- * **************************************************************************
- * @file    i2c_eeprom.h
- * @brief   Abstract functional APIs for managing non-volatile multi-byte read/write operations.
- * **************************************************************************
- */
+// Start header guard
+#ifndef _I2C_EEPROM_H_
 
-#ifndef _I2C_EEPROM_H_              
-#define _I2C_EEPROM_H_              
+// Define header guard
+#define _I2C_EEPROM_H_
 
-#include "types.h"                  
+// Include user defined data types
+#include "types.h"
 
-void i2c_eeprom_write_byte(u8 SlaveAddr, u16 BuffAddr, u8 data);
-u8 i2c_eeprom_read_byte(u8 SlaveAddr, u16 BufferAddr);
-void i2c_eeprom_write_page(u8 SlavAddr, u16 BuffAddr, s8 *P, u8 nbytes);
-void i2c_eeprom_seq_read(u8 SlaveAddr, u16 BuffAdrr, s8 *p, u8 nbytes);
 
+// Write one byte to EEPROM
+void i2c_eeprom_write_byte
+(
+        u8 SlaveAddr,
+        u16 BuffAddr,
+        u8 data
+);
+
+
+// Read one byte from EEPROM
+u8 i2c_eeprom_read_byte
+(
+        u8 SlaveAddr,
+        u16 BufferAddr
+);
+
+
+// Write multiple bytes to EEPROM
+void i2c_eeprom_write_page
+(
+        u8 SlavAddr,
+        u16 BuffAddr,
+        s8 *P,
+        u8 nbytes
+);
+
+
+// Read multiple bytes from EEPROM
+void i2c_eeprom_seq_read
+(
+        u8 SlaveAddr,
+        u16 BuffAdrr,
+        s8 *p,
+        u8 nbytes
+);
+
+
+// End header guard
 #endif
