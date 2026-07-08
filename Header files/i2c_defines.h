32043493d58659089f5f72603fd4c56754ebff3d
Shirisha Palakurla <palakurlashirishagoud@gmail.com>
2026-07-08 23:30:53 +0530

    Update i2c_defines.h

--- Header files/i2c_defines.h
+++ Header files/i2c_defines.h
@@ -1,39 +1,46 @@
-/**
- * **************************************************************************
- * @file    i2c_defines.h
- * @brief   Clock distribution calculations and register mask constants for I2C.
- * **************************************************************************
- */
+// Start header guard
+#ifndef _I2C_DEFINES_H_
 
-#ifndef _I2C_DEFINES_H_              
-#define _I2C_DEFINES_H_              
+// Define header guard
+#define _I2C_DEFINES_H_
 
-/* Hardware Peripheral Pins allocations mapping */
-#define SCL 2                         
-#define SDA 3                         
 
-/* Pin Alternate Function configuration values */
-#define SCL_PIN_FUNC 0X00000010      
-#define SDA_PIN_FUNC 0X00000040      
+// I2C pin numbers
+#define SCL 2   // SCL pin
+#define SDA 3   // SDA pin
 
-/* Frequency Engine Calculations */
-#define FOSC       12000000          /* Quartz Crystal Oscillator input frequency */
-#define CCLK      (FOSC*5)           /* Core CPU processing Clock frequency speed */
-#define PCLK      (CCLK/4)           /* Peripheral Clock distribution speed output */
-#define I2C_SPEED 100000             /* Desired transmission rate target (100kHz) */
-#define LOAD_VAL  ((PCLK/I2C_SPEED)/2) /* Register countdown period calculation divisor */
 
-/* Bit position masks for the I2C Control Set Register (I2CONSET) */
-#define AA   2                       /* Assert Acknowledge Flag */
-#define SI   3                       /* I2C State Interrupt Bit */
-#define STO  4                       /* Assert STOP Bit */
-#define STA  5                       /* Assert START Bit */
-#define I2EN 6                       /* Hardware Engine Interface Enable Bit */
+// I2C pin function values
+#define SCL_PIN_FUNC 0X00000010   // Select SCL function
+#define SDA_PIN_FUNC 0X00000040   // Select SDA function
 
-/* Bit position masks for the I2C Control Clear Register (I2CONCLR) */
-#define AAC    2                     /* Clear Acknowledge flag */
-#define SIC    3                     /* Clear Status Interrupt flag */
-#define STAC   5                     /* Clear START state tracker */
-#define I2ENC  6                     /* Disengage hardware engine block */
 
+// Clock values
+#define FOSC 12000000   // Oscillator frequency
+
+#define CCLK (FOSC*5)   // CPU clock
+
+#define PCLK (CCLK/4)   // Peripheral clock
+
+#define I2C_SPEED 100000   // I2C speed
+
+#define LOAD_VAL ((PCLK/I2C_SPEED)/2)   // I2C clock load value
+
+
+// I2CONSET bit positions
+#define AA   2    // Acknowledge bit
+#define SI   3    // Interrupt flag bit
+#define STO  4    // Stop bit
+#define STA  5    // Start bit
+#define I2EN 6    // I2C enable bit
+
+
+// I2CONCLR bit positions
+#define AAC   2    // Clear acknowledge bit
+#define SIC   3    // Clear interrupt flag
+#define STAC  5    // Clear start bit
+#define I2ENC 6    // Clear I2C enable bit
+
+
+// End header guard
 #endif
